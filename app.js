8483b7c6aa14ea5fd4f797a542306281bb72f901
Francis Avancena <foavancena@gmail.com>
2018-03-08 11:14:00 +0800

    Added socket.io

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const views = require('./routes/routes.js');
-const port = process.env.PORT || 80;
 const app = express();
+const Twit = require('twit');
+const config = require('./twitter_config');
+const T = new Twit(config);
+const server = require('http').Server(app)
+const io = require('socket.io')(server);
+const port = process.env.PORT || 80;
 
 // Middleware
 app.use(bodyParser.urlencoded({
@@ -18,12 +22,36 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Routes
-app.use('/', views);
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
-app.get('*',(req, res) => {
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+
+const stream = T.stream('statuses/sample', {tweet_mode: 'extended'});
+stream.on('tweet', tweet => {
+    if (tweet.lang == 'en' || tweet.lang == 'tl') {
+        if (tweet.text.slice(0,2) != 'RT') {
+            if (tweet.extended_tweet == undefined) {
+                io.emit('tweet', tweet.text + ': ' + tweet.retweet_count);
+                console.log('Short tweet');
+            } else {
+                io.emit('tweet', tweet.extended_tweet.full_text + ': ' + tweet.retweet_count);
+                console.log('Long tweet');
+            }
+        }
+    }
+});
+
+app.get('*', (req, res) => {
     res.render('404');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Application is listening at port ${port}: http://localhost:${port}`);
 });
