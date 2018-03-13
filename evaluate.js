02baff3d2d7c37268196b42313fa25893f0c1435
brenmat <brennanmato@gmail.com>
2018-03-13 15:39:00 +0800

    batch scoring added
    to add labeling

---
+++ evaluate.js
@@ -0,0 +1,98 @@
+// Neural Network
+var scorer = require('./scorer.js');
+
+// Test
+// var fs = require('fs')
+// data = 	JSON.parse(fs.readFileSync('sample.json'));
+
+// API Client
+var twit = require('twit')
+const config = require('./twitter_config');
+const T = new Twit(config);
+
+
+// Sentiment Analysis
+var speakeasy = require('speakeasy-nlp')
+
+// Returns user score and historical tweets for tagging
+function score_user(
+	user_tweet,
+	current_maximum_engagement_rate
+	current_maximum_interaction_rate
+	)
+{
+	var candidate = user_tweet.user;
+	// Removes users with too many/too few users i.e. NOT microinfluencers
+	if ((candidate.followers_count>1000)&&(candidate.followers_count<10000)){
+
+		// Var initialization 
+		var target = candidate.screen_name;
+		var favourites = 0;
+		var retweets = 0;
+		var follow = 0;
+		var posts_with_interactions = 0;
+		var sensitive_content_posts = 0;
+		var historical_tweets = '';
+
+		// Async query for historical tweets
+		T.get('statuses/user_timeline',{screen_name:target,count:200}).
+			then(
+				(response)=>{
+					historical_tweets = JSON.parse(response);
+				}
+		);
+		for (var i = historical_tweets.length - 1; i >= 0; i--) {
+			retweets += historical_tweets[i]['retweet_count'];
+			favourites += historical_tweets[i]['favorite_count'];
+			follow += historical_tweets[i].user.followers_count;
+			if((historical_tweets[i].favorite_count > 0)||(historical_tweets[i].retweet_count > 0)){
+				posts_with_interactions++;
+			}
+			if (historical_tweets[i].possibly_sensitive) {
+				sensitive_content_posts++;
+			}
+		}
+		user_engagement_rate = (favourites+retweets)/follow;
+		content_interaction-rate = (posts_with_interactions)/historical_tweets.length;
+		sensitive_content_rate = sensitive_content_posts/historical_tweets.length;
+		aggregate_sentiment_grade = 0;
+		total_retweets = 0
+		if(user_engagement_rate>0.02){
+			for (var i = historical_tweets.length - 1; i >= 0; i--) {
+				T.get('statuses/retweets/:id',{id:historical_tweets[i].id}).
+					then(
+						(response)=>{
+							sentiment_grade = 0;
+							for (var i = response.length - 1; i >= 0; i--) {
+								aggregate_sentiment_grade += speakeasy.classify(response[i].text);
+							}
+							total_retweets+=response.length;
+						}
+				);
+			}
+		average_sentiment_grade = aggregate_sentiment_grade/total_retweets;
+		grader =  new scorer();
+		return [grader.network.activate([content_interaction-rate,user_engagement_rate,sensitive_content_rate,average_sentiment_grade]),historical_tweets];
+		}
+		else{
+			return 'Unqualified';
+		}
+	}
+	else{
+		return 'Unqualified';
+	}
+}
+
+
+async function batch_async(tweet_list){
+	const promises = tweet_list.map(score_user)
+	results = await Promise.all(promises);
+	for (var i = if(results.length - 1; i >= 0; i--) {
+		if(results[i].length > 1){
+			tag_results(results[i]);
+		}
+	}
+}
+
+// Test
+// batch_async(data);
