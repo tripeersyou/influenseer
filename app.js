81a911fb3cc3c3ff9fc7dfdce7db372eed3c8c7a
Francis Avancena <foavancena@gmail.com>
2018-03-21 08:02:28 +0800

    Added jQuery and Bulma added another route and view with that route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const Twit = require('twit');
 const config = require('./twitter_config');
 const T = new Twit(config);
-
+const stream = T.stream('statuses/sample');
 // Instagram Scraper
 const ig = require('instagram-scraper');
 
@@ -37,29 +37,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// ig.getUserData('jiggyvillanueva').then( data => {
-//     let user = JSON.stringify(data);
-//     console.log(user);
-//     fs.writeFile('data/user.json', user);
-// });
-
-// Routes
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    stream.start();
     socket.on('disconnect', () => {
+        stream.stop();
         console.log('user disconnected');
     });
 });
 
-const stream = T.stream('statuses/sample');
 stream.on('tweet', tweet => {
+    console.log(tweet.text);    
     if ((tweet.lang == 'en' && tweet.user.location == 'Republic of the Philippines ') || tweet.lang == 'tl') {
         if (tweet.user.followers_count > 1000 && tweet.user.followers_count < 10000) {
-            console.log(tweet.text);
             T.get('statuses/user_timeline', {screen_name: tweet.user.screen_name,count: 200,include_rts: false}, (err, tweets) => {
                 io.emit('tweet', tweets);
             });
@@ -69,8 +63,8 @@ stream.on('tweet', tweet => {
 
 app.get('/instagram/:handle', (req, res) => {
 	let user = req.params.handle;
-	ig.getUserData(user, (err, data) => {
-		res.render('ig/show', {data: data});
+	ig.getUserData(user).then( data => {
+		res.render('ig_show', {data: data});
 	});
 });
 
