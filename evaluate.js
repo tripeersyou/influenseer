46522c3b5282c4136721ac4c8394bafca4fdeb57
Brennan <brennanmato@gmail.com>
2018-03-23 20:06:51 +0800

    'it_works'

--- evaluate.js
+++ evaluate.js
@@ -7,84 +7,130 @@ const Twit = require('twit')
 const config = require('./twitter_config');
 const T = new Twit(config);
 
-
-// Sentiment Analysis
+// Sentiment Analysis / NLP
+const natural = require('natural');
+const tokenizer = new natural.WordTokenizer();
 const speakeasy = require('speakeasy-nlp')
 
-
-function evaluate(tweets, id) {
-	let favourites = 0;
-	let retweets = 0;
-	let follow = 0;
-	let posts_with_interactions = 0;
-	let sensitive_content_posts = 0;
-	let historical_tweets = tweets;
-	let sentiment_grade = 0;
-
-	for (let i = historical_tweets.length - 1; i >= 0; i--) {
-		retweets += parseInt(historical_tweets[i]['retweet_count']);
-		favourites += parseInt(historical_tweets[i]['favorite_count']);
-		follow += parseInt(historical_tweets[i].user.followers_count);
-		if ((parseInt(historical_tweets[i].favorite_count) > 0) || (parseInt(historical_tweets[i].retweet_count) > 0)) {
-			posts_with_interactions++;
-		}
-		if (historical_tweets[i].possibly_sensitive) {
-			sensitive_content_posts++;
-		}
-	}
-
-	user_engagement_rate = (favourites + retweets) / follow;
-	content_interaction_rate = (posts_with_interactions) / historical_tweets.length;
-	sensitive_content_rate = sensitive_content_posts / historical_tweets.length;
-	aggregate_sentiment_grade = 0;
-	total_retweets = 0
-
-	console.log(user_engagement_rate);
-	console.log(content_interaction_rate);
-	console.log(sensitive_content_rate);
+class evaluater{
+	evaluate(tweets, id) {
+		let favourites = 0;
+		let retweets = 0;
+		let follow = 0;
+		let posts_with_interactions = 0;
+		let sensitive_content_posts = 0;
+		let is_beauty = false;
+		let is_family = false;
+		let historical_tweets = tweets;
+		let sentiment_grade = 0;
+		let user_engagement_rate = 0;
+		let sensitive_content_rate = 0;
+		let content_interaction_rate = 0;
+		let aggregate_sentiment_grade = 0;
+		let total_retweets = 0
+		let average_sentiment_grade = 0;
+		let tokenizer = new natural.TreebankWordTokenizer();
 
 
-	for (var i = historical_tweets.length - 1; i >= 0; i--) {
-		T.get('statuses/retweets/:id', {
-			id: historical_tweets[i].id_str
-		}).
-		then(response => {
-			sentiment_grade = 0;
-			for (var i = response.length - 1; i >= 0; i--) {
-				aggregate_sentiment_grade += speakeasy.classify(response[i].text);
+		for (let i = historical_tweets.length - 1; i >= 0; i--) {
+			retweets += parseInt(historical_tweets[i]['retweet_count']);
+			favourites += parseInt(historical_tweets[i]['favorite_count']);
+			follow += parseInt(historical_tweets[i].user.followers_count);
+			if ((parseInt(historical_tweets[i].favorite_count) > 0) || (parseInt(historical_tweets[i].retweet_count) > 0)) {
+				posts_with_interactions++;
 			}
-		});
+			if (historical_tweets[i].possibly_sensitive) {
+				sensitive_content_posts++;
+			}
+		}
+
+		user_engagement_rate = (favourites + retweets) / follow;
+		content_interaction_rate = (posts_with_interactions) / historical_tweets.length;
+		sensitive_content_rate = sensitive_content_posts / historical_tweets.length;
+
+		for (var i = historical_tweets.length - 1; i >= 0; i--) {
+			T.get('statuses/retweets/:id', {
+				id: historical_tweets[i].id_str
+			}).
+			then(response => {
+				sentiment_grade = 0;
+				for (var i = response.length - 1; i >= 0; i--) {
+					aggregate_sentiment_grade += speakeasy.classify(response[i].text);
+				}
+			});
+		}
+
+		average_sentiment_grade = aggregate_sentiment_grade / total_retweets;
+
+
+		for (let tweet of historical_tweets){ 
+			let text = tokenizer.tokenize(tweet.full_text);
+			if(this.beauty_tagger(text)){
+				is_beauty = true;
+				break;
+			}
+		}
+
+		for (let tweet of historical_tweets){ 
+			let text = tokenizer.tokenize(tweet.full_text);
+			if(this.family_tagger(text)){
+				is_family = true;
+				break;
+			}
+		}
+
+		let grader = new scorer();
+		return [grader.network.activate([content_interaction_rate, user_engagement_rate, sensitive_content_rate, average_sentiment_grade]),is_beauty,is_family];
 	}
-	console.log(sentiment_grade);
-	average_sentiment_grade = aggregate_sentiment_grade / total_retweets;
-	let grader = new scorer();
-	return grader.network.activate([content_interaction_rate, user_engagement_rate, sensitive_content_rate, average_sentiment_grade]);
 
-}
+	beauty_tagger(text) {
 
-
-// Returns user score and historical tweets for tagging
-
-function batch(tweet_list) {
-	const results = evaluate(tweet_list.slice);
-	console.log(results);
-}
-
-function debug(historical_tweets) {
-	historical_tweets = historical_tweets.slice(0, 20);
-	for (var i = historical_tweets.length - 1; i >= 0; i--) {
-		T.get('statuses/retweets/:id', {
-			count: 100,
-			id: historical_tweets[i].id_str,
-		}, (err, response) => {
-			sentiment_grade = 0;
-			console.log(response);
-		});
+		// Filter by positive
+		let beauty = ['lipstick','make-up','powder','salon','facial','face','skin','body'];
+		let is_beauty = false;
+		for (var i = text.length - 1; i >= 0; i--) {
+			if(beauty.includes(text[i])){
+				is_beauty =  true;
+				break;
+			}
+		}
+		return is_beauty;
 	}
 
+	family_tagger(text){
+		let family = ['son','kids','brother','father','mother','sister','pamilya','daughter','love','care'];
+		let is_family = false;
+		for (var i = text.length - 1; i >= 0; i--) {
+			if(family.includes(text[i])){
+				is_family =  true;
+				break;
+			}
+		}
+		return is_family;
+
+	}
+
+	debug(historical_tweets) {
+		historical_tweets = historical_tweets.slice(0, 20);
+		for (var i = historical_tweets.length - 1; i >= 0; i--) {
+			tagger(historical_tweets[i])
+		}
+		for (var i = historical_tweets.length - 1; i >= 0; i--) {
+			T.get('statuses/retweets/:id', {
+				count: 100,
+				id: historical_tweets[i].id_str,
+			}, (err, response) => {
+				sentiment_grade = 0;
+				console.log(response);
+			});
+		}
+
+	}
 }
 
 // Test
+
 var fs = require('fs')
 data = JSON.parse(fs.readFileSync('data/twitter.json'));
-debug(data);
+eva = new evaluater();
+console.log(eva.evaluate(data));
